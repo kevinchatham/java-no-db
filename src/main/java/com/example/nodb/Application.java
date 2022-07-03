403b57117f6c23b7b26e1937ee286f015c79c6fb
kevinchatham <40923272+kevinchatham@users.noreply.github.com>
2022-07-03 13:18:12 -0500

    comments

--- src/main/java/com/example/nodb/Application.java
+++ src/main/java/com/example/nodb/Application.java
@@ -2,7 +2,8 @@ package com.example.nodb;
 
 public class Application {
 	public static void main(String[] args) {
-		for (Item item : Database.ITEMS) {
+		// consumption
+		for (Item item : Database.ITEMS_TABLE) {
 			System.out.println("Id: " + item.id);
 			System.out.println("Name: " + item.name);
 		}
--- src/main/java/com/example/nodb/Database.java
+++ src/main/java/com/example/nodb/Database.java
@@ -1,9 +1,14 @@
 package com.example.nodb;
 
 public final class Database {
-    public final static Item[] ITEMS = new Item[] {
+    public final static Item[] ITEMS_TABLE = new Item[] {
             new Item(0, "Ali"),
             new Item(1, "Rue"),
-            new Item(2, "Sam")
+            new Item(2, "Sam"),
+            // etc
     };
+
+    // public final static X[] ...
+
+    // etc
 }
